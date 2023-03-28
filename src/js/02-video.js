0506fb1c84ce402d4c0078cf32bedd2a1c20df54
RenTensei <scs.bon7@gmail.com>
2023-03-29 02:16:35 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,17 +6,14 @@ const TIME_PLAYED = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
 
+const savedTime = JSON.parse(localStorage.getItem(TIME_PLAYED));
+
+if (!savedTime) {
+  localStorage.setItem(TIME_PLAYED, '0');
+}
+
 // sync player time from previous session
-(() => {
-  player
-    .setCurrentTime(localStorage.getItem(TIME_PLAYED))
-    .then(() => {
-      console.log('synced player time from last session!');
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-})();
+player.setCurrentTime(localStorage.getItem(TIME_PLAYED));
 
 function timeupdateCallback({ seconds }) {
   localStorage.setItem(TIME_PLAYED, seconds);
